96d55d93c6d8377daacd34e3a6ba8964c0a7f3d6
“e <13116075090@sina.cn>
2016-05-02 20:18:01 +0800

    change

--- assets/js/gaya.js
+++ assets/js/gaya.js
@@ -105,24 +105,24 @@
 /*
  * Show disqus comments
  */
-// jQuery(document).ready(function() {
+jQuery(document).ready(function() {
 
-//   jQuery(".post").fitVids();
+  jQuery(".post").fitVids();
 
-//   // Load discus comment
-//   function initDisqusComments(){
+  // Load discus comment
+  function initDisqusComments(){
 
-//     if(config.disqus_shortname != '' && config.disqus_shortname != null && config.disqus_shortname != undefined) {
-//       var disqus_shortname = config.disqus_shortname;
-//       (function() {
-//       var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-//       dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-//       (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-//       })();
-//     }else {
-//       alert("Please check Disqus short name configuration on your _config.yml");
-//     }
-//   }
+    if(config.disqus_shortname != '' && config.disqus_shortname != null && config.disqus_shortname != undefined) {
+      var disqus_shortname = config.disqus_shortname;
+      (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
+    }else {
+      alert("Please check Disqus short name configuration on your _config.yml");
+    }
+  }
 
 //   $('.load-view').click(function(){
 //     initDisqusComments();
