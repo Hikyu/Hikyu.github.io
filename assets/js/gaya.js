84cf56acd369204e452e46b08a0761ca9cc446cb
“e <13116075090@sina.cn>
2016-05-02 18:49:28 +0800

    change

--- assets/js/gaya.js
+++ assets/js/gaya.js
@@ -130,21 +130,21 @@
 //   });
 // });
 
-$(document).ready(function() {     
-    jQuery(".post").fitVids();
-    if(config.disqus_shortname != '' && config.disqus_shortname != null && config.disqus_shortname != undefined) {
-      var disqus_shortname = config.disqus_shortname;
-      (function() {
-      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      })();
-    }else {
-      alert("Please check Disqus short name configuration on your _config.yml");
-    }
-  } 
-  // $(this).fadeOut(200);
-}); 
+// $(document).ready(function() {     
+//     jQuery(".post").fitVids();
+//     if(config.disqus_shortname != '' && config.disqus_shortname != null && config.disqus_shortname != undefined) {
+//       var disqus_shortname = config.disqus_shortname;
+//       (function() {
+//       var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+//       dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+//       (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+//       })();
+//     }else {
+//       alert("Please check Disqus short name configuration on your _config.yml");
+//     }
+//   } 
+//   // $(this).fadeOut(200);
+// }); 
 /*
  * Scroll to top button
  */
